6f7191ac2fc61ddda2181f3fec978c78de37a488
ndg <3140713095@qq.com>
2023-07-03 14:40:19 +0800

    通过 json 添加数据

--- springboot-mybatis/src/main/java/com/ndg/controller/EmployeeController.java
+++ springboot-mybatis/src/main/java/com/ndg/controller/EmployeeController.java
@@ -1,12 +1,20 @@
 package com.ndg.controller;
 
+import com.github.pagehelper.PageInfo;
+import com.ndg.common.Result;
+import com.ndg.common.StatusCode;
 import com.ndg.entity.Employee;
 import com.ndg.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.validation.Valid;
 import java.util.List;
 //import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +31,10 @@ public class EmployeeController {
     @Autowired//从spring容器中取出业务逻辑层对象，并赋值给该对象
             // 运行时，IOC 容器会提供给类型的 bean对象，并赋值给该变量  -  依赖注入
     EmployeeService employeeService;
+    @RequestMapping("/getPage")
+    public Result<PageInfo<Employee>> getPage(Integer currentPage, Integer pageSize){
+        return employeeService.getPage(currentPage,pageSize);
+    }
 
     @RequestMapping("/getId")
     @ResponseBody
@@ -42,11 +54,12 @@ public class EmployeeController {
 
     //添加一行数据 /add?id=207
     @RequestMapping("/add")
-    public void addEmployee(Integer id ,String first_name){
+    public Employee addEmployee(Integer id ,String first_name){
         Employee employee = new Employee();
         employee.setEmployeeId(id);
         employee.setFirstName(first_name);
         employeeService.addEmployee(employee);
+        return employee;
     }
 
     //删除一行数据 /delete?id=207
@@ -62,5 +75,25 @@ public class EmployeeController {
         employee.setEmployeeId(id);
         employee.setFirstName(first_name);
         return employeeService.updateEmployee(employee);
+    }
+
+    @PostMapping("/add")//post类型的请求
+    public Result<String> addEmployee(@Valid @RequestBody Employee employee, BindingResult result){
+        //判断验证规则是否包含错误
+        if(result.hasErrors()){
+            //获取所有错误信息
+            List<ObjectError> allErrors = result.getAllErrors();
+            //将错误信息拼接为一个字符穿返回
+            StringBuilder sb= new StringBuilder();// 该对象是可变字符串对象
+            for (ObjectError allError : allErrors){
+                //获取粗无消息的提示
+                String message = allError.getDefaultMessage();
+                sb.append(message).append("  : ");
+            }
+            String mes = sb.toString();
+            return new Result<>(false, StatusCode.ERROR,"验证失败",mes);
+        }
+        employeeService.addEmployee(employee);
+        return new Result<>(true ,StatusCode.OK,"添加成功",employee.toString());
     }
 }
--- springboot-mybatis/src/main/java/com/ndg/entity/Employee.java
+++ springboot-mybatis/src/main/java/com/ndg/entity/Employee.java
@@ -1,6 +1,11 @@
 package com.ndg.entity;
 
 import lombok.*;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotBlank;
+
+import javax.validation.constraints.Pattern;
+
 
 /**
  * @className: Employee
@@ -16,9 +21,15 @@ import lombok.*;
 @AllArgsConstructor // 全参构造
 public class Employee {
     private Integer employeeId;
+    @NotBlank(message = "firstName 不能为空")
+    @Length(min = 2,max = 10,message = "firstName 必须在2-10之间")
     private String firstName;
+    @NotBlank(message = "lastName 不能为空")
+    @Length(min = 2,max = 10,message = "lastName 必须在2-10之间")
     private String lastName;
     private String email;
+    @NotBlank(message = "电话号码不能为空")
+    @Pattern(regexp = "^1(3[0-9]|5[012356789]|7[1235678]|8[0-9])\\d{8}$")
     private String phoneNumber;
     private String jobId;
     private double salary;
